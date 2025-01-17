2285f02c288528606adc37a83426931865ac2489
iago <iagomauricio7@gmail.com>
2025-01-17 01:03:22 -0300

    feito template

--- setup/urls.py
+++ setup/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 from rest_framework import routers
 
 from cafezinho.views import ContribuicaoViewSet, ContribuinteViewSet
@@ -10,5 +11,6 @@ router.register(r"contribuicoes", ContribuicaoViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('home/', TemplateView.as_view(template_name="index.html"), name='index'),
 ]
